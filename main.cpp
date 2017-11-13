8f09aafdafc8a62b7f098900cb4f3e85c9e72938
v6854384 <32385043+v6854384@users.noreply.github.com>
2017-11-13 14:15:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,17 @@ using namespace std;
 int main (int argc, char*argv[])
 {
    string str1, str2, str3;
-   str1=argv[1];
-   str2=argv[2];
-   str3=str1+str2;
-   cout << str3 << endl;
+   if(argc > 1){
+                str1=argv[1];
+                if(argc > 2){
+                        if(argc > 3) cout << "Mnogo dannyh"<<endl;
+                        else {
+                                str2=argv[2];
+                                str3=str1+str2;
+                                cout << str3 << endl;
+                        }
+
+                }
+                else cout << "Nehvataet dannyh" << endl;
+   }
 }
